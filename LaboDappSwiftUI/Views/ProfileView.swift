4aa7d300f5e4dbce015099653901b1f9bf0f8fc7
tsutsuken <592846+tsutsuken@users.noreply.github.com>
2022-04-12 11:50:32 +0900

    refactor: Move connect function to ProfileView

--- LaboDappSwiftUI/Views/ProfileView.swift
+++ LaboDappSwiftUI/Views/ProfileView.swift
@@ -10,13 +10,49 @@ import SwiftUI
 struct ProfileView: View {
     @Environment(\.container) var container: Container
     
+    private func copyParingUri() {
+        print("copyParingUri")
+        do {
+            if let uri = try container.walletManager.generateParingUri() {
+                print("uri: \(uri)")
+                UIPasteboard.general.string = uri
+            }
+        } catch {
+            print("generateParingUri error: \(error)")
+        }
+    }
+    
+    private func disconnectWallet() {
+        container.walletManager.disconnect()
+    }
+    
     var body: some View {
         NavigationView {
             List {
-                HStack {
-                    Text("Address")
-                        .bold()
-                    Text("\(container.walletManager.address() ?? "No Address")")
+                if container.walletManager.session == nil {
+                    HStack {
+                        Text("Copy paring uri")
+                            .onTapGesture {
+                                copyParingUri()
+                            }
+                    }
+                } else {
+                    Section {
+                        HStack {
+                            Text("Address")
+                                .bold()
+                            Text("\(container.walletManager.address() ?? "No Address")")
+                        }
+                    }
+                    Section {
+                        HStack {
+                            Text("Disconnect wallet")
+                                .foregroundColor(.red)
+                                .onTapGesture {
+                                    disconnectWallet()
+                                }
+                        }
+                    }
                 }
             }
             .navigationTitle("Profile")
--- LaboDappSwiftUI/Views/RequestsView.swift
+++ LaboDappSwiftUI/Views/RequestsView.swift
@@ -15,18 +15,6 @@ struct RequestsView: View {
         isShowingAlertRequestSent = true
     }
     
-    private func copyParingUri() {
-        print("copyParingUri")
-        do {
-            if let uri = try container.walletManager.generateParingUri() {
-                print("uri: \(uri)")
-                UIPasteboard.general.string = uri
-            }
-        } catch {
-            print("generateParingUri error: \(error)")
-        }
-    }
-    
     private func personalSign() {
         container.walletManager.personalSign()
     }
@@ -39,23 +27,16 @@ struct RequestsView: View {
         container.repositories.chainLinkRepository.transfer(amount: 1)
     }
     
-    private func disconnectWallet() {
-        container.walletManager.disconnect()
-    }
-    
     var body: some View {
         NavigationView {
             VStack {
                 if container.walletManager.session == nil {
                     List {
                         HStack {
-                            Text("Copy paring uri")
-                                .onTapGesture {
-                                    copyParingUri()
-                                }
+                            Text("Wallet not connected")
                         }
                     }
-                    .navigationTitle("Connect To Wallet")
+                    .navigationTitle("Requests")
                 } else {
                     List {
                         HStack {
@@ -77,12 +58,6 @@ struct RequestsView: View {
                                 .onTapGesture {
                                     transferLink()
                                     showAlertRequestSent()
-                                }
-                        }
-                        HStack {
-                            Text("Disconnect wallet")
-                                .onTapGesture {
-                                    disconnectWallet()
                                 }
                         }
                     }
