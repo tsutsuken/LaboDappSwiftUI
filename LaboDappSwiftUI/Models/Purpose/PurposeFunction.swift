87cada18809f0ac239fa60325e2ac8b115a6ef6c
tsutsuken <592846+tsutsuken@users.noreply.github.com>
2022-04-22 13:03:57 +0900

    refactor: Separate PurposeFunction and  PurposeResponse

---
+++ LaboDappSwiftUI/Models/Purpose/PurposeFunction.swift
@@ -0,0 +1,58 @@
+//
+//  PurposeFunction.swift
+//  LaboDappSwiftUI
+//
+//  Created by Ken Tsutsumi on 2022/04/22.
+//
+
+import Foundation
+import web3
+import BigInt
+
+enum PurposeFunction {
+    public struct getPurpose: ABIFunction {
+        public static let name = "purpose"
+        public let gasPrice: BigUInt?
+        public let gasLimit: BigUInt?
+        public var contract: EthereumAddress
+        public let from: EthereumAddress?
+        
+        public init(contract: EthereumAddress,
+                    from: EthereumAddress? = nil,
+                    gasPrice: BigUInt? = nil,
+                    gasLimit: BigUInt? = nil) {
+            self.contract = contract
+            self.from = from
+            self.gasPrice = gasPrice
+            self.gasLimit = gasLimit
+        }
+        
+        public func encode(to encoder: ABIFunctionEncoder) throws {}
+    }
+    
+    public struct setPurpose: ABIFunction {
+        public static let name = "setPurpose"
+        public let gasPrice: BigUInt?
+        public let gasLimit: BigUInt?
+        public var contract: EthereumAddress
+        public let from: EthereumAddress?
+        
+        public let purpose: String
+        
+        public init(contract: EthereumAddress,
+                    from: EthereumAddress? = nil,
+                    gasPrice: BigUInt? = nil,
+                    gasLimit: BigUInt? = nil,
+                    purpose: String) {
+            self.contract = contract
+            self.from = from
+            self.gasPrice = gasPrice
+            self.gasLimit = gasLimit
+            self.purpose = purpose
+        }
+        
+        public func encode(to encoder: ABIFunctionEncoder) throws {
+            try encoder.encode(purpose)
+        }
+    }
+}
---
+++ LaboDappSwiftUI/Models/Purpose/PurposeResponse.swift
@@ -0,0 +1,20 @@
+//
+//  PurposeResponse.swift
+//  LaboDappSwiftUI
+//
+//  Created by Ken Tsutsumi on 2022/04/22.
+//
+
+import Foundation
+import web3
+
+public enum PurposeResponse {
+    public struct purposeResponse: ABIResponse, MulticallDecodableResponse {
+        public static var types: [ABIType.Type] = [ String.self ]
+        public let value: String
+        
+        public init?(values: [ABIDecoder.DecodedValue]) throws {
+            self.value = try values[0].decoded()
+        }
+    }
+}
